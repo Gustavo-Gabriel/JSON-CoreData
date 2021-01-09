b57d5ab809381eeca9b0b9b0475923ed14ba0c0e
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-01-09 00:24:30 -0400

    JSON terminado

--- JSON+CoreData/controller/ViewController.swift
+++ JSON+CoreData/controller/ViewController.swift
@@ -8,20 +8,42 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var petitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let url = URL(string: API_KEY){
+            if let data = try? Data(contentsOf: url){
+                parse(json: data)
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return petitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        
+        let petition = petitions[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = String (petition.signatureCount)
+        return cell
+        
     }
-
-
+    
+    func parse(json: Data) {
+        
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json){
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    
+    }
 }
 
---
+++ JSON+CoreData/model/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  JSON+CoreData
+//
+//  Created by Gustavo Anjos on 09/01/21.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
